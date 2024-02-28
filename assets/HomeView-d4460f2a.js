31750a1cb451ba4c1fa2b12dbd0b2c93dfc145fc
brownie-o <145078343+brownie-o@users.noreply.github.com>
2024-02-28 21:50:22 +0000

    Deploying to gh-pages from @ brownie-o/cookie_vuetify-project3@ab417ed6a1da333b9f2bd9a1a315c48b41f5d8af 🚀

---
+++ assets/HomeView-d4460f2a.js
@@ -0,0 +1,1 @@
+import{o as e,e as o,_ as n}from"./index-568aef07.js";function c(t,a){return e(),o("h1",null,"AdminHome")}const r={},m=n(r,[["render",c]]);export{m as default};
---
+++ assets/favicon-5ff3d8e0.js
@@ -0,0 +1,1 @@
+const e=""+new URL("favicon-ecd18e30.ico",import.meta.url).href;export{e as _};
